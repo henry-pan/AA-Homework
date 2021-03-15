2391c168a5527f7c30c76c3aba9a355199211837
Henry <henry-pan@users.noreply.github.com>
2021-03-15 02:00:04 -0700

    Complete intro to canvas

---
+++ W9D1/practice.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById('mycanvas');
+  canvas.height = 500;
+  canvas.width = 500;
+  const ctx = canvas.getContext('2d');
+  
+  ctx.fillStyle = 'red';
+  ctx.fillRect(0, 0, 500, 500);
+  
+  ctx.beginPath();
+  ctx.arc(50, 50, 20, 0, 2 * Math.PI, true);
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+  
+  ctx.fillStyle = 'purple';
+  ctx.fill();
+  
+});
